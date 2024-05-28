6c793ff4a118c172d0cda5ab4398a3e5cd635a80
ivrude <136907387+ivrude@users.noreply.github.com>
2024-05-28 19:24:23 +0300

    Ad trigers saving to db

--- get_data.py
+++ get_data.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import SYNCHRONOUS
 
@@ -5,8 +7,8 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 token = "Xwx1R1lV2_dcGr2AvCDHPoRvpq90n5hlCwutM2Zw3ZQXKzmURxj1q6Vs1BQx9fIiL499NRDpzyZwW0Gv8uGnBA=="
 org = "Chornobyl"
 bucket = "Graf"
-url = "http://192.168.0.95:8086"
-host="host3"
+url = "http://192.168.0.218:8086"
+
 # Ініціалізація клієнта InfluxDB
 client = InfluxDBClient(url=url, token=token, org=org)
 query_api = client.query_api()
@@ -17,7 +19,7 @@ def fetch_last_value(field_name, host):
     query = f'''
     from(bucket: "{bucket}")
       |> range(start: -6h)
-      |> filter(fn: (r) => r["_measurement"] == "test_4")
+      |> filter(fn: (r) => r["_measurement"] == "test_5")
       |> filter(fn: (r) => r["_field"] == "{field_name}")
       |> filter(fn: (r) => r["host"] == "{host}")
       |> last()
@@ -28,30 +30,32 @@ def fetch_last_value(field_name, host):
             return record.get_value()
     return None
 
-def zapis(name, value):
-    data = f"test_4,host={host} {name}={value}"
+def zapis(name, value, host):
+    data = f"test_5,host={host} {name}={value}"
     write_api.write(bucket=bucket, org=org, record=data)
 
 # Отримання останніх значень для "high_radiation" і "high_electr"
-def triger(number):
-    Current = fetch_last_value(f"Current_{number}", host)
-    State = fetch_last_value(f"State_{number}", host)
-    print(f"Current_{number}: {Current}")
-    print(f"State_{number}: {State}")
+def triger(number, host):
+    Current = fetch_last_value(f"current_amount_{number}", host)
+    State = fetch_last_value(f"state_amount_{number}", host)
+    print(f"current_amount_{number}: {Current}")
+    print(f"state_amount_{number}: {State}")
     if Current is not None and State is not None:
         if Current > 2 * State and Current < 3 * State:
-            zapis(f"triger_{number}", 2)
+            zapis(f"triger_{number}", 2, host)
         elif Current >= 3 * State:
-            zapis(f"triger_{number}", 3)
+            zapis(f"triger_{number}", 3, host)
         else:
-            zapis(f"triger_{number}", 1)
+            zapis(f"triger_{number}", 1, host)
     else:
         print(f"Failed to fetch data for Current_{number} or State_{number}")
+while True:
+    hosts = ['host1', 'host2', 'host3', 'host4']
+    for host in hosts:
+        for i in range(1, 5):
+            triger(i, host)
 
-triger(1)
-triger(2)
-triger(3)
-triger(4)
+    sleep(60)
 
 # Закриття з'єднання з InfluxDB
 client.close()
