256212b0610559a3e19bc5fa0156fef46aeb0194
ivrude <136907387+ivrude@users.noreply.github.com>
2024-07-25 15:25:05 +0300

    streaming video fix

--- stream video.py
+++ stream video.py
@@ -1,36 +1,52 @@
 from flask import Flask, Response
 import cv2
-import time
+import threading
+import queue
 from config import rtsp_camrera
+
 app = Flask(__name__)
 
-def generate_frames():
-    camera = cv2.VideoCapture(rtsp_camrera)
+
+frame_queue = queue.Queue(maxsize=10)
+
+def capture_frames():
+    cap = cv2.VideoCapture(rtsp_camrera)
+    if not cap.isOpened():
+        print("Error: Unable to open video source")
+        return
+
     while True:
-
-        success, frame = camera.read()
-        if not success:
-            print("Failed to grab frame, retrying...")
-            camera.release()
-            camera = cv2.VideoCapture(rtsp_camrera)
-            time.sleep(1)
-            continue
-
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 30]
-        ret, buffer = cv2.imencode('.jpg', frame, encode_param)
+        ret, frame = cap.read()
         if not ret:
-            print("Failed to encode frame, retrying...")
-            time.sleep(1)
+            print("Failed to grab frame")
             continue
-        frame = buffer.tobytes()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        time.sleep(0.11)
+
+        if frame_queue.full():
+            for i in range(10):
+                frame_queue.get()
+                i=i+1
+
+        frame_queue.put(frame)
+
+def generate():
+    while True:
+        if not frame_queue.empty():
+            frame = frame_queue.get()
+            ret, buffer = cv2.imencode('.jpg', frame)
+            if not ret:
+                print("Failed to encode frame")
+                continue
+
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(),
+    return Response(generate(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
+    capture_thread = threading.Thread(target=capture_frames)
+    capture_thread.start()
     app.run(host='0.0.0.0', port=8080)
