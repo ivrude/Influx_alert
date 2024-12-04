140046ee2ea98ec1b8e653db8eec2fccaba886a6
ivrude <136907387+ivrude@users.noreply.github.com>
2024-12-04 13:48:08 +0200

    delete second chanel + add lamda

--- cache.py
+++ cache.py
@@ -90,7 +90,7 @@ conn = sqlite3.connect("Test_docker/Test.db")
 cursor = conn.cursor()
 
 # Список таблиць для обробки
-tables = ["host3_e", "host3"]
+tables = ["host3"]
 
 # Запис останніх даних з кожної таблиці в InfluxDB
 
@@ -104,29 +104,18 @@ while True:
                 current_amounts = last_record[:4]
                 state_amounts = last_record[4:8]
                 triger_state = last_record[8:12]
-                sumar = (
-                    last_record[8] +
-                    last_record[9] +
-                    last_record[10] +
-                    last_record[11]
-                )
+                lamda = last_record[13]
                 timestamp = datetime.datetime.utcnow() + datetime.timedelta(
                     hours=2
                 )  # Adjusting for UTC+3
                 zapis("radio_state", radio_state, table, timestamp)
+                zapis("lamda", lamda, table, timestamp)
                 for i, value in enumerate(current_amounts, start=1):
-                    zapis(f"current_amount_{i}", value, table, timestamp)
+                    zapis(f"current_amount_{i}", float(value), table, timestamp)
                 for i, value in enumerate(state_amounts, start=1):
                     zapis(f"state_amount_{i}", value, table, timestamp)
-                if sumar > 1:
-                    for i, value in enumerate(triger_state, start=1):
-                        if triger_state[i - 1] == 0:
-                            zapis(f"triger_state_{i}", value, table, timestamp)
-                        else:
-                            zapis(f"triger_state_{i}", 2, table, timestamp)
-                else:
-                    for i, value in enumerate(triger_state, start=1):
-                        zapis(f"triger_state_{i}", value, table, timestamp)
+                for i, value in enumerate(triger_state, start=1):
+                    zapis(f"triger_state_{i}", value, table, timestamp)
         else:
             zapis("radio_state", radio_state, table, timestamp)
     resend_cached_data()
