0b8dcb0b16263e315b9a86d53da5645c430f2884
ivrude <136907387+ivrude@users.noreply.github.com>
2024-12-09 10:05:46 +0200

    Change hand sending info

--- influx_web.py
+++ influx_web.py
@@ -1,4 +1,5 @@
-#app to send info to influx not from radiometr auto mut by hands
+#app to send info to influx not from radiometr auto but by hands
+
 from flask import Flask, render_template, request, redirect, url_for
 from influxdb_client import InfluxDBClient
 from influxdb_client.client.write_api import ASYNCHRONOUS
@@ -6,39 +7,34 @@ from config import token_influx, url_influx
 
 app = Flask(__name__)
 
-# configuration InfluxDB
+# Configuration InfluxDB
 ORG = "Chornobyl"
 BUCKET = "Graf"
 TOKEN = token_influx
 client = InfluxDBClient(url=url_influx, token=TOKEN, org=ORG)
 write_api = client.write_api(write_options=ASYNCHRONOUS)
 
-
-# write to InfluxDB
+# Write to InfluxDB
 def zapis(name, value):
-    data = f"test_6,host=host3 Self_fon_{name}={value}"
+    data = f"test_6,host=host3 {name}={value}"
     write_api.write(bucket=BUCKET, org=ORG, record=data)
 
-# page whith table
+# Main page with table
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        name = request.form["name"]
-        value = request.form["value"]
-        zapis(name, value)
+        # Get data from the form
+        kuzov = request.form.get("body")
+        kolesa = request.form.get("wheels")
+        dno_mashini = request.form.get("bottom")
+
+        # Send each field to InfluxDB
+        zapis("Кузов", kuzov)
+        zapis("Колеса", kolesa)
+        zapis("Дно_машини", dno_mashini)
+
         return redirect(url_for("index"))
     return render_template("index.html")
-
-# sending info
-@app.route("/send", methods=["POST"])
-def send():
-    data = request.form
-    for (
-        name,
-        value,
-    ) in data.items():
-        zapis(name, value)
-    return redirect(url_for("index"))
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
