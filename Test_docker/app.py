1001338c4fcae568e020f8d98c3b48174487e4e1
ivrude <136907387+ivrude@users.noreply.github.com>
2025-05-28 12:16:17 +0300

    Update in Chornobyl

--- Test_docker/app.py
+++ Test_docker/app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-connection = sqlite3.connect("Test.db")
+connection = sqlite3.connect("/home/admin-user/Influx_alert/Test_docker/Test.db")
 cursor = connection.cursor()
 
 # Створюємо таблицу host3_e
@@ -14,21 +14,21 @@ cursor.execute('''
                        INTEGER
                        PRIMARY
                            KEY,
-                   curent_amount_1
+                   vymir_1
                        REAL,
-                   curent_amount_2
+                   vymir_2
                        REAL,
-                   curent_amount_3
+                   vymir_3
                        REAL,
-                   curent_amount_4
+                   vymir_4
                        REAL,
-                   state_amount_1
+                   fon_1
                        REAL,
-                   state_amount_2
+                   fon_2
                        REAL,
-                   state_amount_3
+                   fon_3
                        REAL,
-                   state_amount_4
+                   fon_4
                        REAL,
                    triger_1
                        INTEGER,
@@ -44,7 +44,7 @@ cursor.execute('''
                         INTEGER
                )
                ''')
-conn = sqlite3.connect("Test.db")
+conn = sqlite3.connect("/home/admin-user/Influx_alert/Test_docker/Test.db")
 cursor = conn.cursor()
 
 @app.route('/')
@@ -53,18 +53,18 @@ def index():
 
 @app.route('/latest_data')
 def latest_data():
-    conn = sqlite3.connect("Test.db")
+    conn = sqlite3.connect("/home/admin-user/Influx_alert/Test_docker/Test.db")
     cursor = conn.cursor()
-    cursor.execute("SELECT * FROM host3_e ORDER BY id DESC LIMIT 1")
+    cursor.execute("SELECT * FROM host3_e ORDER BY rowid DESC LIMIT 1")
     row = cursor.fetchone()
     conn.close()
 
     if row:
-        keys = ["id", "curent_amount_1", "curent_amount_2", "curent_amount_3", "curent_amount_4",
+        keys = ["curent_amount_1", "curent_amount_2", "curent_amount_3", "curent_amount_4",
                 "state_amount_1", "state_amount_2", "state_amount_3", "state_amount_4",
-                "triger_1", "triger_2", "triger_3", "triger_4", "triger_0", "radiometr_state"]
+                "triger_1", "triger_2", "triger_3", "triger_4", "radiometr_state","id", "triger_0"]
         return jsonify(dict(zip(keys, row)))
     return jsonify({})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
